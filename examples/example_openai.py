57dcb2e3fc3f13e1194f1488d6fc27157a1dc6ce
Marco Perini <perinim.98@gmail.com>
2024-10-16 11:11:01 +0200

    chore: add simpler building description

--- examples/example_openai.py
+++ examples/example_openai.py
@@ -7,15 +7,12 @@ load_dotenv()
 
 # Specify the user prompt
 building_description = """
-The building (external ref: 'OB103'), with coordinates 33.9614, -118.3531, has a total area of 500 m². It has three zones, each with its own air temperature sensor.
-The building has an electrical meter that monitors data of a power sensor. An HVAC equipment serves all three zones and its power usage is measured by a power sensor.
-
-Timeseries IDs and unit of measure of the sensors:
-- Building power consumption: '1b3e-29dk-8js7-f54v' in watts.
-- HVAC power consumption: '29dh-8ks3-fvjs-d92e' in watts.
-- Temperature sensor zone 1: 't29s-jk83-kv82-93fs' in celsius.
-- Temperature sensor zone 2: 'f29g-js92-df73-l923' in celsius.
-- Temperature sensor zone 3: 'm93d-ljs9-83ks-29dh' in celsius.
+I have a building located in Bolzano.
+It has 3 floors and each floor has 1 office.
+There are 2 rooms in each office and each room has three sensors:
+- Temperature sensor;
+- Humidity sensor;
+- CO sensor.
 """
 
 # Create an instance of BrickSchemaGraph with a predefined provider
