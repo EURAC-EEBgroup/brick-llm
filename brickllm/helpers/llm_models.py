bf3cf6bab72add3f449a3932d51e360a76c18da3
Giudice7 <rocco.giudice7@gmail.com>
2024-12-24 09:50:58 +1100

    feat: implementing ollama 3.2 through ChatOllama models

--- brickllm/helpers/llm_models.py
+++ brickllm/helpers/llm_models.py
@@ -2,6 +2,7 @@ from typing import Union
 
 from langchain.chat_models.base import BaseChatModel
 from langchain_anthropic import ChatAnthropic
+from langchain_ollama import ChatOllama
 from langchain_community.llms import Ollama
 from langchain_fireworks import ChatFireworks
 from langchain_openai import ChatOpenAI
@@ -23,6 +24,8 @@ def _get_model(model: Union[str, BaseChatModel]) -> BaseChatModel:
 
     if model == "openai":
         return ChatOpenAI(temperature=0, model="gpt-4o")
+    elif model == "ollama3.2":
+        return ChatOllama(model="llama3.2")
     elif model == "anthropic":
         return ChatAnthropic(temperature=0, model_name="claude-3-sonnet-20240229")
     elif model == "fireworks":
@@ -31,5 +34,8 @@ def _get_model(model: Union[str, BaseChatModel]) -> BaseChatModel:
         )
     elif model == "llama3.1:8b-brick":
         return Ollama(model="llama3.1:8b-brick-v8")
+    elif model == "hf.co/Giudice7/llama32-3B-brick-demo:latest":
+        return Ollama(model="hf.co/Giudice7/llama32-3B-brick-demo:latest")
+
     else:
-        raise ValueError(f"Unsupported model type: {model}")
+        raise ValueError(f"Unsupported model type: {model}. Load your own BaseChatModel if this one is not supported.")
